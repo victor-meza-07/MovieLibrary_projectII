aa85708d662422e758e8475fc07e0a5bad40852d
victor-meza-07 <victord.meza@gmail.com>
2020-03-31 12:07:40 -0500

    Added js to facilitate html generation and page dynamicity

--- Client/app.js
+++ Client/app.js
@@ -1,3 +1,6 @@
+var temporaryImage = "https://www.istockphoto.com/resources/images/PhotoFTLP/Essential-images-we-love-1055891344.jpg";
+
+
 (function($){
     function processForm( e ){
         var dict = {
@@ -21,6 +24,95 @@
 
         e.preventDefault();
     }
-
+    //On Document ready, wer are going to call Navigate to Home View;
+    $(NavigateToHomeView());
     $('#my-form').submit( processForm );
 })(jQuery);
+
+
+function NavigateToHomeView()
+{
+    let homeView_html = GenerateHomeViewHtml();
+    DisplayView(homeView_html);
+}
+
+function GenerateHomeViewHtml()
+{
+    let homeViewHtml = ``;
+    //start from the inside and work your way out.
+    //in order:
+        //Random Picker
+        let randomPicker_html = GenerateRandomPcikerHTML();
+        //Add Featured Conetent
+        let featuredFilm_html = GenerateFeaturedFilmHTML();
+        //Add Navigation
+        let navigation_html = GenerateNavigationHTML();
+    
+    homeViewHtml = `<div class="contentContainerStyle"><div class="homeContainer">${randomPicker_html}${featuredFilm_html}${navigation_html}</div></div>`;
+    return homeViewHtml;
+}
+
+function GenerateNavigationHTML()
+{
+    let html = `<div class="navigation-container">`+
+                `<div class="row justify-content-center">`+
+                `<div class="btn-group navigation-btn-group">`+
+                `<div class="col-4">`+
+                `<button class="navigation-btn" onclick="NavigateToHomeView()"><i class="fa">&#xf015;</i></button>`+
+                `</div>`+
+                `<div class="col-4">`+
+                `<button class="navigation-btn" onclick="NavigateToSearchView()"><i class="fa fa-search" aria-hidden="true"></i></button>`+
+                `</div>`+
+                `<div class="col-4">`+
+                `<button class="navigation-btn" onclick="NavigateToLibraryView()"><i class="fa fa-list" aria-hidden="true"></i></button>`+
+                `</div></div></div></div>`;
+    return html;
+} 
+function GenerateFeaturedFilmHTML()
+{
+    let html = ``;
+    let movie_obj = GetMovieObject();
+    html = `<div class="featured-container">`+
+            `<img id="featured-img" class="featured-img" src="${movie_obj.ImageURL}">`+
+            `<button id="featured-btn" type="button" class="btn btn-primary feautred-btn">See Featured Film Details</button></div>`;
+    return html;
+}
+function GetMovieObject()
+{
+    let movieObject = {ImageURL: temporaryImage};
+    //TODO:
+    //.get jquery function to be exact.
+    //this is where we write some more ajax request functions;
+    return movieObject;
+}
+function GenerateRandomPcikerHTML()
+{
+    let randomCollection = [];
+    randomCollection = GenerateRandomCollection(4);
+    let title_html = `<div class="row"><div class="col-12"><div class="randompickHeader-container"><h1 class="randompickHeader">Random Picks</h1></div></div></div>`;
+    let radnom1_html = `<div class="row"><div class="col-6"><div class="randomtitle-container"><p class="randomPickTitle">${randomCollection[0]}</p></div></div><div class="col-6"><div class="randomtitle-container"><p class="randomPickTitle">${randomCollection[1]}</p></div></div></div>`;
+    let random2_html = `<div class="row"><div class="col-6"><div class="randomtitle-container"><p class="randomPickTitle">${randomCollection[2]}</p></div></div><div class="col-6"><div class="randomtitle-container"><p class="randomPickTitle">${randomCollection[3]}</p></div></div></div>`;
+    let html = `<div class="randomPickContainer">${title_html}${radnom1_html}${random2_html}</div>`;
+    return html;
+}
+function GenerateRandomCollection(NumberOfrandomPicks)
+{
+    //this is where we call an ajax get function;
+    let randomCollection = [];
+    for(let i = 0; i < NumberOfrandomPicks; i++)
+    {
+        //TODO:
+
+        //this is where we pull the collection from Ajax. 
+        //for testing
+        randomCollection[i] = i;
+    }
+
+    return randomCollection;
+}
+
+
+function DisplayView(html)
+{
+    document.getElementById("dynamicContent").innerHTML=html;
+}
